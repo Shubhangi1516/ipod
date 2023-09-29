c38a9ce29fd6e1a6adea603189929a88b1333f1d
Shubhangi Shree <shubhangishree25@gmail.com>
2023-09-29 23:35:33 +0530

    commit

---
+++ src/assets/songs/songs.js
@@ -0,0 +1,12 @@
+import music1 from "./TheShow.mp3";
+import music2 from "./TheShow.mp3";
+import music3 from "./TheShow.mp3";
+
+const songs = {
+	music1,
+	music2,
+	music3,
+};
+
+export default songs;
+
